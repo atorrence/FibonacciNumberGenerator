7281294e09faee4904c4fdc60a9e986af7718882
Anita Torrence <anita.torrence@gmail.com>
2017-07-04 09:57:12 -0400

    Added models and views

---
+++ FibonacciNumberGenerator/Models/Home.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FibonacciNumberGenerator.Models
+{
+    public class Home
+    {
+        public int InputNumber { get; set; }
+    }
+}
---
+++ FibonacciNumberGenerator/Models/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FibonacciNumberGenerator.Models
+{
+    public class Result
+    {
+        public int InputNumber { get; set; }
+        //Method to find the nth number of the Fibonacci Sequence
+        public int GetSequence()
+        {
+            int previous = 0;
+            int next = 1;
+            int result = 0;
+
+            for (int i = 3; i <= InputNumber; i++)
+            {
+                if (InputNumber <= 1)
+                {
+                    result = InputNumber;
+                }
+                else
+                {
+                    result = previous + next;
+                    previous = next;
+                    next = result;
+                }
+            }
+            return result;
+        }
+    }
+}
